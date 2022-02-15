00d1dff61ad55321e0d76e1f0f69a626809b91d3
Nikita Bhatnagar <bhatnagarnikita.02@gmail.com>
2022-02-15 15:24:35 +0530

    search and filter updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const cardsSection = document.querySelector(".cards");
 const container = document.querySelector(".container");
 const container2 = document.querySelector(".container2");
+const errorMsg=document.querySelector(".error-msg");
 let theme = "dark";
 
 //get all countries data
@@ -24,11 +25,12 @@ window.addEventListener("load", getAll);
 //display data
 function display(countries) {
   try {
+    errorMsg.style.display="none";
     cardsSection.innerHTML = "";
     countries.forEach((country) => {
       const cardMarkup = `<a href="#${
         country.name.common
-      }"><div class="card ${theme}">
+      }"><div class="card">
         <img src="${country.flags.png}" alt="flag" />
         <h4 class="name">${country.name.common}</h4>
         <div class="info">
@@ -64,6 +66,7 @@ const search = document.querySelector(".search");
 const searchInput = document.querySelector(".search-input");
 const filter = document.querySelector(".filter");
 const filterInput = document.querySelector(".filter-input");
+const options=document.querySelector(".options");
 let cards;
 mode.addEventListener("click", toggleTheme);
 function toggleTheme(e) {
@@ -79,47 +82,17 @@ function toggleTheme(e) {
   moon.classList.toggle("far");
   body.classList.toggle("dark-bg");
   body.classList.toggle("light-bg");
-  navbarWrapper.classList.toggle("dark");
-  navbarWrapper.classList.toggle("light");
-  search.classList.toggle("dark");
-  search.classList.toggle("light");
-  searchInput.classList.toggle("dark");
-  searchInput.classList.toggle("light");
-  filter.classList.toggle("dark");
-  filter.classList.toggle("light");
-  filterInput.classList.toggle("dark");
-  filterInput.classList.toggle("light");
-  container2.classList.toggle("dark");
-  container2.classList.toggle("light");
-  if (document.querySelector(".card")) {
-    cards = document.querySelectorAll(".card");
-    cards.forEach((card) => {
-      card.classList.toggle("dark");
-      card.classList.toggle("light");
-    });
-  }
-  if (container2.innerHTML !== "") {
-    container2.querySelector(".container-detail").classList.toggle("light");
-    container2.querySelector(".container-detail").classList.toggle("dark");
-    container2.querySelector(".border").classList.toggle("light");
-    container2.querySelector(".border").classList.toggle("dark");
-    container2.querySelector(".back-btn").classList.toggle("light");
-    container2.querySelector(".back-btn").classList.toggle("dark");
-    if (container2.querySelector(".box")) {
-      container2.querySelectorAll(".box").forEach((box) => {
-        box.classList.toggle("dark");
-        box.classList.toggle("light");
-      });
-    }
-  }
+  body.classList.toggle("light");
+  body.classList.toggle("dark");
 }
 
 //filter countries by region
 const getFiltered = async function (e) {
   displayLoader();
   try {
-    const region = e.currentTarget.value;
-    if (region === "all") {
+    const region = e.target.textContent;
+    
+    if (region === "All") {
       getAll();
       return;
     }
@@ -131,7 +104,22 @@ const getFiltered = async function (e) {
     console.log(err);
   }
 };
-filterInput.addEventListener("input", getFiltered);
+options.addEventListener("click", getFiltered);
+
+
+function toggleRegionList(e){
+  options.classList.toggle("display");
+  if( filterInput.querySelector("i").classList.contains("fa-angle-right")){
+  filterInput.querySelector("i").classList.remove("fa-angle-right");
+  filterInput.querySelector("i").classList.add("fa-angle-down");
+
+  }
+  else{
+    filterInput.querySelector("i").classList.add("fa-angle-right");
+  filterInput.querySelector("i").classList.remove("fa-angle-down");
+  }
+}
+filter.addEventListener("click",toggleRegionList);
 
 //search country
 const searchCountry = async function (e) {
@@ -143,14 +131,42 @@ const searchCountry = async function (e) {
       return;
     }
     const res = await fetch(`https://restcountries.com/v3.1/name/${query}`);
+    if(!res.ok){
+      errorMsg.style.display="block";
+      throw new Error("no countries found!!");
+    
+    }
+    else{
     const data = await res.json();
     hideLoader();
     display(data);
+    }
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
   }
 };
+const searchByFullCountryName=async function(e){
+  if(e.key==="Enter"){
+    const query=e.target.value;
+    if(query.trim()!=="")
+    {
+      displayLoader();
+      const res=await fetch(`https://restcountries.com/v3.1/name/${query}?fullText=true`);
+      if(!res.ok){
+        errorMsg.style.display="block";
+      }
+      else{
+      const data=await res.json();
+    
+      hideLoader();
+      display(data);
+      }
+    }
+
+  }
+}
 searchInput.addEventListener("input", searchCountry);
+searchInput.addEventListener("keydown",searchByFullCountryName);
 
 //displa detailed data
 function displayDetailed(data) {
@@ -164,9 +180,9 @@ function displayDetailed(data) {
       currStr.push(data[0].currencies[key].name);
     }
     currStr = currStr.join(", ");
-    const markup = `<div class="container-detail ${theme}">
+    const markup = `<div class="container-detail">
 
-<a href="#"><button class="back-btn ${theme}" type="button">
+<a href="#"><button class="back-btn" type="button">
   <i class="fas fa-arrow-left"></i>
   Back
 </button></a>
@@ -219,7 +235,7 @@ function displayDetailed(data) {
         </p>
       </div>
     </div>
-    <div class="border ${theme}">
+    <div class="border">
       
     </div>
   </div>
@@ -242,7 +258,7 @@ const getCountryFromBorder = function (borders) {
       fetch(`https://restcountries.com/v3.1/alpha/${border}`)
         .then((res) => res.json())
         .then((data) => {
-          const nameMarkup = `<a href="#${data[0].name.common}"><span class="box ${theme}">${data[0].name.common}</span></a>`;
+          const nameMarkup = `<a href="#${data[0].name.common}"><span class="box">${data[0].name.common}</span></a>`;
           borderDiv.insertAdjacentHTML("beforeend", nameMarkup);
         });
     });
